692757235c93e0b2e7b8d0401faac60d50d82e3a
GetALife <74838478+GetALife@users.noreply.github.com>
2021-11-27 15:27:23 +0200

    Update server.js

--- c2/server.js
+++ c2/server.js
@@ -5,7 +5,7 @@ var fs = require('fs');
 
 const server = http.createServer();
 server.on('request', (request, response) => {
-    var ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress || null;
+    var ip = request.headers['x-forwarded-for'] || request.socket.remoteAddress || null;
     fs.writeFile('ip.txt', `${ip}`)
     console.log("test")
 }).listen(port);
